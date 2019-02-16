73b465d5cffb38b795503ec5c578fc5f206eeedb
Jonatan Machado <jonatanmachado35@gmail.com>
2019-02-16 05:09:36 -0200

    arquivos organizados e iniciando redux

--- aulas/modulo3/mod03/src/App.js
+++ aulas/modulo3/mod03/src/App.js
@@ -1,28 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//importação externo
+import React from "react";
+import { Provider } from "react-redux";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+//importação interno
+import store from "./store";
+import TodoList from "./TodoList";
+
+const App = () => (
+  <Provider store={store}>
+    <h1>Hello World</h1>
+    <TodoList />
+  </Provider>
+);
 
 export default App;
