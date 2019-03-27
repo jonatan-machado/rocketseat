4a828ab7cbd4ff1791968c4803800a6df401373f
Jonatan Machado <jonatanmachado35@gmail.com>
2019-03-27 13:50:50 -0300

    lista de todos

--- aulas/modulo3/mod03/src/App.js
+++ aulas/modulo3/mod03/src/App.js
@@ -1,11 +1,14 @@
+//importação externa
 import React from "react";
 import { Provider } from "react-redux";
 
+//importação interna
 import store from "./store";
+import TodoList from "./TodoList";
 
 const App = () => (
   <Provider store={store}>
-    <h1>Hello World</h1>
+    <TodoList />
   </Provider>
 );
 export default App;
