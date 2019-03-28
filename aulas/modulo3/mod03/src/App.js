da8df403b07fbcf1daac2f24ee900a51b21410e9
Jonatan Machado <jonatanmachado35@gmail.com>
2019-03-28 03:27:35 -0300

    reactotron feito da forma atual

--- aulas/modulo3/mod03/src/App.js
+++ aulas/modulo3/mod03/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Provider } from "react-redux";
 
 //importação interna
+import "./config/reactototron";
 import store from "./store";
 import TodoList from "./TodoList";
 
